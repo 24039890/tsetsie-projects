f497663747caa6beea6336a9748cb804c884ea17
24039890 <163397479+24039890@users.noreply.github.com>
2024-05-18 13:50:53 +0200

    second commit

---
+++ src/z1/Z1.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package z1;
+ 
+
+import java.util.Scanner;
+class Z1{ 
+    
+ public static void main( String [] args){
+     
+   
+     
+  Scanner div = new Scanner( System.in);
+  
+  int X;
+     
+   System.out.println(" enter the value of x"); 
+  
+   X = div.nextInt();  
+     
+  if( X % 4 == 0) System.out.println(X  + " is the value for the next code "); 
+
+
+
+
+
+
+for(  X = 10 ;    X <= 50;    X ++){
+    
+    X=50;
+    
+    
+    System.out.println(X);
+}  
+     
+     
+ 
+    
+    
+    
+    
+    
+    
+    
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
